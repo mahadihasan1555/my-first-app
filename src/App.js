0879b965fabc56d0df7d683495942ad656e6ba46
mahadihasan1555 <mahadihasan1555@gmail.com>
2020-09-03 21:06:49 +0600

    components example

--- src/App.js
+++ src/App.js
@@ -5,22 +5,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+     
+      <Actor></Actor>
+      <Actor></Actor>
+      <Actor></Actor>
+      <Actor></Actor>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
+}
+
+//components
+function Actor(){
+  const actorStyle = {
+    border: '.1rem solid red',
+    margin: '10px 150px'
+
+  }
+  return <div style = {actorStyle}>
+    <h1>I am boss</h1>
+    <h3>I have done my WOrk</h3>
+  </div>
 }
 
 export default App;
