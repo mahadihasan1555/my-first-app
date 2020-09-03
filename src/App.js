83246c1a70c5b183ba3f9069c3f61dc4362e7ce3
mahadihasan1555 <mahadihasan1555@gmail.com>
2020-09-03 22:39:05 +0600

    Pass data through Props

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const actorName = ["mahadi", "mahdi hasan", "hasan mahadi", "jahid hasan"]
   return (
     <div className="App">
      
-      <Actor></Actor>
-      <Actor></Actor>
-      <Actor></Actor>
-      <Actor></Actor>
+      <Actor name = {actorName[0]}></Actor>
+      <Actor name = "nahid"></Actor>
+      <Actor name = {actorName[2]}></Actor>
+      <Actor name = "sahid"></Actor>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
@@ -17,17 +18,20 @@ function App() {
   );
 }
 
-//components
-function Actor(){
+//components and pass data(props)
+function Actor(props){
+  console.log(props.name)
   const actorStyle = {
     border: '.1rem solid red',
     margin: '10px 150px'
 
   }
-  return <div style = {actorStyle}>
-    <h1>I am boss</h1>
-    <h3>I have done my WOrk</h3>
-  </div>
+  return (
+    <div style = {actorStyle}>
+      <h1>I am boss- {props.name}</h1>
+      <h3>I have done my WOrk</h3>
+   </div>
+  )
 }
 
 export default App;
